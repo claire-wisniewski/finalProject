6d21b68c5602eea9cc5c0b3874fab2c51ec2c003
Emma Brunsell <ebrunsell@wisc.edu>
2026-03-19 10:26:17 -0500

    Updated reading, includes unzip

--- readData.sh
+++ readData.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
 wget https://www.kaggle.com/api/v1/datasets/download/azizkasimov/wind-turbine-scada-data-for-early-fault-detection
+
+unzip wind-turbine-scada-data-for-early-fault-detection
+
+mv Wind\ Farm\ A/ WindFarmA
+
+mv Wind\ Farm\ B/ WindFarmB
+
+mv Wind\ Farm\ C/ WindFarmC
